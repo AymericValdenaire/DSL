e84fadb6a7deb32020974d4bac65ac34b7926423
Aymeric Valdenaire <aymericvaldenaire@hotmail.fr>
2020-02-01 12:50:55 +0100

    Brick kernel

--- embedded_DSL/src/main/java/kernel/model/Brick.java
+++ embedded_DSL/src/main/java/kernel/model/Brick.java
@@ -1,11 +1,32 @@
 package kernel.model;
 
 import kernel.generator.Visitable;
-import lombok.Data;
+import kernel.generator.Visitor;
+import lombok.Getter;
 
-@Data
+@Getter
 public abstract class Brick implements Visitable {
 
-    final String name;
+    private final String name;
+    private final int pin;
 
+    public Brick(String name, int pin) {
+        this.name = name;
+        this.pin = pin;
+    }
+
+    @Override
+    public String initCode() {
+        return String.format("def brick %s = %d;", name, pin);
+    }
+
+    @Override
+    public String declarationVarCode(){
+        return String.format("\nint %s = %d;", name, pin);
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
 }
