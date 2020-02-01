54f79c2005dd272c2a7218fe0375012eb52728a8
Aymeric Valdenaire <aymericvaldenaire@hotmail.fr>
2020-02-01 22:36:59 +0100

    fix Visitor

--- embedded_DSL/src/main/java/kernel/generator/Visitor.java
+++ embedded_DSL/src/main/java/kernel/generator/Visitor.java
@@ -3,20 +3,23 @@ package kernel.generator;
 import kernel.ArduinoApp;
 import kernel.logic.BoolExpression;
 import kernel.logic.State;
+import kernel.logic.statements.Statement;
 import kernel.logic.statements.action.Assignement;
+import kernel.logic.statements.transition.Exception;
 import kernel.logic.statements.transition.Transition;
 import kernel.model.DigitalValue;
 import kernel.model.brick.Brick;
 import kernel.model.brick.Serial;
 
 public abstract class Visitor<T> {
+  
     public abstract void visit(Brick brick);
     public abstract void visit(State state);
     public abstract void visit(ArduinoApp arduinoApp);
     public abstract void visit(DigitalValue digitalValue);
     public abstract void visit(BoolExpression boolExpression);
-    public abstract void visit(Transition transition);
     public abstract void visit(Serial serial);
-    public abstract void visit(Assignement assignement);
+    public abstract void visit(Exception e);
+    public abstract void visit(Statement statement);
 }
 
